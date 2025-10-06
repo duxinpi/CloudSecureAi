449c774cb897a581bc7611a9c5e6d87ed123567c
cloudxhposh-ux <ddxhposh@gmail.com>
2025-10-06 15:15:01 -0700

    Update Convert_Word_to_PDF.py

--- Convert_Word_to_PDF.py
+++ Convert_Word_to_PDF.py
@@ -2,6 +2,7 @@ from docx2pdf import convert
 import os
 
 def convert_word_to_pdf(input_path, output_path=None):
+    
     if not os.path.exists(input_path):
         raise FileNotFoundError(f"The file '{input_path}' does not exist.")
 
@@ -10,5 +11,4 @@ def convert_word_to_pdf(input_path, output_path=None):
         print(f"Conversion successful! PDF saved to: {output_path or os.path.dirname(input_path)}")
     except Exception as e:
         print(f"Error during conversion: {e}")
-
-convert_word_to_pdf("fileName") # TO DO, get file name from user
+        # TO DO, get file name from user
